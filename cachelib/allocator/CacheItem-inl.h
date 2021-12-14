pmem/CacheLib

Implemented async Item movement between tiers

cachelib/allocator/CacheItem-inl.h
@@ -265,6 +265,21 @@ bool CacheItem<CacheTrait>::isNvmEvicted() const noexcept {
 }
 
 template <typename CacheTrait>
+void CacheItem<CacheTrait>::markNotReady() noexcept {
+  ref_.markNotReady();
+}
+
+template <typename CacheTrait>
+void CacheItem<CacheTrait>::unmarkNotReady() noexcept {
+  ref_.unmarkNotReady();
+}
+
+template <typename CacheTrait>
+bool CacheItem<CacheTrait>::isNotReady() const noexcept {
+  return ref_.isNotReady();
+}
+
+template <typename CacheTrait>
 void CacheItem<CacheTrait>::markIsChainedItem() noexcept {
   XDCHECK(!hasChainedItem());
   ref_.markIsChainedItem();

cachelib/allocator/CacheItem.h
@@ -241,6 +241,14 @@ class CACHELIB_PACKED_ATTR CacheItem {
   bool isNvmEvicted() const noexcept;
 
   /**
+   * Marks that the item is migrating between memory tiers and
+   * not ready for access now. Accessing thread should wait.
+   */
+  void markNotReady() noexcept;
+  void unmarkNotReady() noexcept;
+  bool isNotReady() const noexcept;
+
+  /**
    * Function to set the timestamp for when to expire an item
    * Employs a best-effort approach to update the expiryTime. Item's expiry
    * time can only be updated when the item is a regular item and is part of

igchor/CacheLib-1

cachelib/allocator/CacheItem-inl.h
@@ -265,6 +265,21 @@ bool CacheItem<CacheTrait>::isNvmEvicted() const noexcept {
 }
 
 template <typename CacheTrait>
+void CacheItem<CacheTrait>::markIncomplete() noexcept {
+  ref_.markIncomplete();
+}
+
+template <typename CacheTrait>
+void CacheItem<CacheTrait>::unmarkIncomplete() noexcept {
+  ref_.unmarkIncomplete();
+}
+
+template <typename CacheTrait>
+bool CacheItem<CacheTrait>::isIncomplete() const noexcept {
+  return ref_.isIncomplete();
+}
+
+template <typename CacheTrait>
 void CacheItem<CacheTrait>::markIsChainedItem() noexcept {
   XDCHECK(!hasChainedItem());
   ref_.markIsChainedItem();

cachelib/allocator/CacheItem.h
@@ -242,6 +242,14 @@ class CACHELIB_PACKED_ATTR CacheItem {
   bool isNvmEvicted() const noexcept;
 
   /**
+   * Marks that the item is migrating between memory tiers and
+   * not ready for access now. Accessing thread should wait.
+   */
+  void markIncomplete() noexcept;
+  void unmarkIncomplete() noexcept;
+  bool isIncomplete() const noexcept;
+
+  /**
    * Function to set the timestamp for when to expire an item
    * Employs a best-effort approach to update the expiryTime. Item's expiry
    * time can only be updated when the item is a regular item and is part of