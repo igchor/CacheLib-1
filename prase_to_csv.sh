3c3b23bc4d51c33eb248faa0366415909782f8fe
Igor Chorążewicz <igor.chorazewicz@intel.com>
2022-03-18 15:03:03 +0100

    Create prase_to_csv.sh

---
+++ prase_to_csv.sh
@@ -0,0 +1,15 @@
+in=`cat`
+
+echo "${in}" | grep 'Hit Ratio' | sed 's/Hit Ratio[: ]*\([0-9.%]*\)/Hit Ratio, \1/'
+echo "${in}" | grep 'Cache Find API latency p50 ' | rev | cut -d " " -f 2 | rev | awk '{print "find p50,"$1}'
+echo "${in}" | grep 'Cache Find API latency p90 ' | rev | cut -d " " -f 2 | rev | awk '{print "find p90,"$1}'
+echo "${in}" | grep 'Cache Find API latency p99 ' | rev | cut -d " " -f 2 | rev | awk '{print "find p99,"$1}'
+echo "${in}" | grep 'Cache Find API latency p999 ' | rev | cut -d " " -f 2 | rev | awk '{print "find p999,"$1}'
+
+echo "${in}" | grep 'Cache Allocate API latency p50 ' | rev | cut -d " " -f 2 | rev | awk '{print "allocate p50,"$1}'
+echo "${in}" | grep 'Cache Allocate API latency p90 ' | rev | cut -d " " -f 2 | rev | awk '{print "allocate p90,"$1}'
+echo "${in}" | grep 'Cache Allocate API latency p99 ' | rev | cut -d " " -f 2 | rev | awk '{print "allocate p99,"$1}'
+echo "${in}" | grep 'Cache Allocate API latency p999 ' | rev | cut -d " " -f 2 | rev | awk '{print "allocate p999,"$1}'
+
+echo "${in}" | grep -E '^get[ :]*' | sed 's/get[ :]*\([0-9,]*\)\/s[ ,0-9%.a-z:]*/\1/' | sed 's/,/./g'
+echo "${in}" | grep -E '^set[ :]*' | sed 's/set[ :]*\([0-9,]*\)\/s[ ,0-9%.a-z:]*/\1/' | sed 's/,/./g'
