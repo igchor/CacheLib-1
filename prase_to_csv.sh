ea95365bea43a03fbaf3d883d671ac11bc19cae8
Igor Chorążewicz <igor.chorazewicz@intel.com>
2022-03-21 16:21:43 +0100

    Update prase_to_csv.sh

--- prase_to_csv.sh
+++ prase_to_csv.sh
@@ -15,5 +15,5 @@ echo "${in}" | grep 'Cache Allocate API latency p999 ' | rev | cut -d " " -f 2 |
 echo "${in}" | grep -E '^get[ :]*' | sed 's/get[ :]*\([0-9,]*\)\/s[ ,0-9%.a-z:]*/\1/' | sed 's/,/./g' | awk '{print "get throughput,"$1}'
 echo "${in}" | grep -E '^set[ :]*' | sed 's/set[ :]*\([0-9,]*\)\/s[ ,0-9%.a-z:]*/\1/' | sed 's/,/./g' | awk '{print "set throughput,"$1}'
 
-# cat $(find . -type f -name "*.csv" | head -n 1) | datamash -t, transpose | head -n 1^C
-# for f in $(find . -type f -name "*.csv"); do cat $f | datamash -t, transpose | tail -n 1 ; done
+# cat $(find . -type f -name "*.csv" | head -n 1) | datamash -t, transpose | head -n 1 | awk '{print "benchmark-name,"$0}'
+# for f in $(find . -type f -name "*.csv"); do echo $f && cat $f | datamash -t, transpose | tail -n 1 | awk '{print $0}' ; done
