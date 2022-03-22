c4a2909d4f15dadd2e4d10b0f04b7746f04e7387
Igor Chorążewicz <igor.chorazewicz@intel.com>
2022-03-22 12:17:18 +0100

    Update prase_to_csv.sh

--- prase_to_csv.sh
+++ prase_to_csv.sh
@@ -12,8 +12,8 @@ echo "${in}" | grep 'Cache Allocate API latency p90 ' | rev | cut -d " " -f 2 | 
 echo "${in}" | grep 'Cache Allocate API latency p99 ' | rev | cut -d " " -f 2 | rev | awk '{print "allocate p99,"$1}'
 echo "${in}" | grep 'Cache Allocate API latency p999 ' | rev | cut -d " " -f 2 | rev | awk '{print "allocate p999,"$1}'
 
-echo "${in}" | grep -E '^get[ :]*' | sed 's/get[ :]*\([0-9,]*\)\/s[ ,0-9%.a-z:]*/\1/' | sed 's/,/./g' | awk '{print "get throughput,"$1}'
-echo "${in}" | grep -E '^set[ :]*' | sed 's/set[ :]*\([0-9,]*\)\/s[ ,0-9%.a-z:]*/\1/' | sed 's/,/./g' | awk '{print "set throughput,"$1}'
+echo "${in}" | grep -E '^get[ :]*' | sed 's/get[ :]*\([0-9,]*\)\/s[ ,0-9%.a-z:]*/\1/' | sed 's/,//g' | awk '{print "get throughput,"$1}'
+echo "${in}" | grep -E '^set[ :]*' | sed 's/set[ :]*\([0-9,]*\)\/s[ ,0-9%.a-z:]*/\1/' | sed 's/,//g' | awk '{print "set throughput,"$1}'
 
 # cat $(find . -type f -name "*.csv" | head -n 1) | datamash -t, transpose | head -n 1 | awk '{print "benchmark-name,"$0}'
 # for f in $(find . -type f -name "*.csv"); do echo "$f," && cat $f | datamash -t, transpose | tail -n 1 | awk '{print $0}' ; done
