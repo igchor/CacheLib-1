26a2a8f9ce04567af61a3937348b9231ac60139b
Igor Chorążewicz <igor.chorazewicz@intel.com>
2022-04-12 07:30:27 -0400

    Update docker

---
+++ docker/install-cachelib-deps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2022, Intel Corporation
+
+git clone https://github.com/pmem/CacheLib CacheLib
+cd CacheLib
+git checkout main
+git cherry-pick 2561f45f7864535852205b43af407937bf9454c6
+cd ..
+
+sed 's/sudo //' -i ./CacheLib/contrib/prerequisites-centos8.sh
+./CacheLib/contrib/prerequisites-centos8.sh
+
+for pkg in zstd googleflags googlelog googletest sparsemap fmt folly fizz wangle fbthrift ;
+do
+    sudo ./CacheLib/contrib/build-package.sh -j -I /opt/ "$pkg"
+done
+
+rm -rf CacheLib
+
