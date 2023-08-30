fd2d9377c15faad33b06e7fd940c445c5c888c0b
Igor Chorazewicz <igor.chorazewicz@intel.com>
2023-08-30 16:11:33 -0700

    Fix docker

--- grafana/run_docker.sh
+++ grafana/run_docker.sh
@@ -2,4 +2,4 @@
 
 sh stop_workload.sh
 
-docker run --privileged --name=cachebench -v $PWD/..:/opt/workspace:z -w /opt/workspace/ -e http_proxy=$http_proxy -e https_proxy=$https_proxy -it test_cachelib sh /opt/workspace/grafana/run_workload.sh $1
+docker run --privileged --name=cachebench -v $PWD/..:/opt/workspace:z -w /opt/workspace/ -e http_proxy=$http_proxy -e https_proxy=$https_proxy -it ghcr.io/pmem/cachelib:centos-8streams-devel sh /opt/workspace/grafana/run_workload.sh $1
