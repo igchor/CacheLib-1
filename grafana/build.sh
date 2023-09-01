103b4724f839a1f216087503b75d69f561c489d6
Igor Chorążewicz <igor.chorazewicz@intel.com>
2023-09-01 11:47:01 -0700

    Add build script

---
+++ grafana/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p /opt/workspace/build
+cd /opt/workspace/build
+
+source /opt/rh/gcc-toolset-12/enable
+cmake ../cachelib -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt
+
+make -j
