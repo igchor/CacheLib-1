3c995d12bf8e02838b7f3b7fe16208575a9295f2
Igor Chorążewicz <igor.chorazewicz@intel.com>
2023-09-20 09:46:13 -0700

    update monitor

--- grafana/cachebench_monitor.sh
+++ grafana/cachebench_monitor.sh
@@ -5,9 +5,15 @@ source ./config.sh
 host_public_ip=$(hostname -I | awk '{print $1}')
 echo "Using the '$host_public_ip' IP as the IP for 'pushgateway'"
 
+wipe_count=0
 while true
 do
-   curl --noproxy '*' -s -X PUT "http://$PUSHGATEWAY_SERVER_IP/api/v1/admin/wipe"
+   if [ "$wipe_count" -eq 10 ]; then
+      curl --noproxy '*' -s -X PUT "http://$PUSHGATEWAY_SERVER_IP/api/v1/admin/wipe"
+      wipe_count=0
+   fi
+
+   wipe_count=$((wipe_count+1))
 
    timestamp=$(cat $PWD/../stats/timestamp)
 
